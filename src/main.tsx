4fd28305c8dadba6145057236cd84cbebb64c00e
= <viktor.rajzer1@gmail.com>
2026-07-09 14:28:09 +0500

    [+] add Tanstack quality for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import { App } from './App'
 import { ToastProvider } from './components/Toast'
 import { ThemeProvider } from './providers/theme-provider'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
